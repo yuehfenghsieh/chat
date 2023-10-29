95f2fb4a46e51a507cb06147d32e6459564fe028
yuehfenghsieh <jason70160@gmail.com>
2023-10-29 21:56:25 +0800

    version01

---
+++ chat.py
@@ -0,0 +1,34 @@
+#讀取檔案
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return(lines)
+
+#格式轉換
+def convert(lines):
+	new = []
+	person = None
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person:
+			new.append(person + ':' + line)
+	return(new)
+
+#寫入檔案
+def write_file(filename, lines):
+	with open(filename, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+def main():
+	lines = read_file('input.txt')
+	lines = convert(lines)
+	write_file('output.txt', lines)
+main()
